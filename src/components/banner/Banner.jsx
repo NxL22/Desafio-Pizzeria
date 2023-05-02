e233c80b0d6f35ca9025ad215ae12872ee202446
neil aular <neilaular01@protonmail.com>
2023-05-01 23:05:32 -0400

    arreglado de acentos

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -3,7 +3,7 @@ import './banner.css'
 const Banner = () => {
     return (
         <div className="headerText">
-            <h1>¡Pizzeria A Modo Mio!</h1>
+            <h1>¡Pizzeria A Modo Mío!</h1>
             <p className='textBanner'>Tenemos el mejor sabor del planeta</p>
         </div>
 
